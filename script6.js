d3403e666ae5d0848fbf3a1b6d0f5445b0bf9fd2
Pokmand <manudesj@msn.com>
2019-04-30 15:22:38 +0200

    avant correction

---
+++ script6.js
@@ -0,0 +1,13 @@
+var testApp = angular.module('app',[]);
+
+testApp.controller('testCtrl', function($scope){
+  $scope.show = function(){
+    $scope.p1=false;
+    $scope.p2=true;
+
+  }
+  $scope.show = function(){
+  $scope.p1=false;
+  $scope.p2=true;
+  }
+});
